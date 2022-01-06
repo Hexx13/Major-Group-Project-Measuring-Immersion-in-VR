312a6e6f902a65d2042293dbce3d47b00db264d2
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-01-06 18:55:32 +0000

    Implement Jumping

---
+++ Prototype/Assets/PlayerController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using Unity.XR.CoreUtils;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.XR.Interaction.Toolkit;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private InputActionReference jumpActionReference;
+    [SerializeField] private float jumpForce = 500;
+
+    private XROrigin _xrOrigin;
+    private CapsuleCollider _collider;
+    private Rigidbody _body;
+
+    private bool _isGrounded=true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _body = GetComponent<Rigidbody>();
+        jumpActionReference.action.performed += OnJump;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var center = _xrOrigin.CameraInOriginSpacePos;
+        _collider.center = new Vector3(center.x, _collider.center.y, center.z);
+        _collider.height = _xrOrigin.CameraInOriginSpaceHeight;
+    }
+
+     void OnCollisionEnter(Collision collision)
+     {
+         if (collision.gameObject.tag == "Floor")
+             _isGrounded = true;
+     }
+
+    private void OnJump(InputAction.CallbackContext obj)
+    {
+        if (!_isGrounded) return;
+        _body.AddForce(Vector3.up * jumpForce);
+        _isGrounded = false;
+    }
+}
