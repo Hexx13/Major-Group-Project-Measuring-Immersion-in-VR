50d04fb6221b1b2f5b73f073ee3fccd199528ea3
Egino200 <71835027+Egino200@users.noreply.github.com>
2022-01-07 14:56:19 +0000

    implementing walking audio

--- Prototype/Assets/script/PlayerController.cs
+++ Prototype/Assets/script/PlayerController.cs
@@ -16,7 +16,7 @@ public class PlayerController : MonoBehaviour
     private XROrigin _xrOrigin;
     private CapsuleCollider _collider;
     private Rigidbody _body;
-
+    public AudioSource audioPlayer;
     private bool _isGrounded=true;
     
     // Start is called before the first frame update
@@ -24,11 +24,13 @@ public class PlayerController : MonoBehaviour
     {
         _body = GetComponent<Rigidbody>();
         jumpActionReference.action.performed += OnJump;
+       
     }
 
     // Update is called once per frame
     void Update()
     {
+        footstep();
         var center = _xrOrigin.CameraInOriginSpacePos;
         _collider.center = new Vector3(center.x, _collider.center.y, center.z);
         _collider.height = _xrOrigin.CameraInOriginSpaceHeight;
@@ -39,11 +41,24 @@ public class PlayerController : MonoBehaviour
          if (collision.gameObject.tag == "Floor")
              _isGrounded = true;
 
-        if (collision.gameObject.tag=="Prop")
+         if (collision.gameObject.tag=="Prop")
         Physics.IgnoreCollision(collision.collider, gameObject.GetComponent<Collider>());     
      }
 
-    private void OnJump(InputAction.CallbackContext obj)
+     void footstep()
+     {
+      
+         if(_isGrounded && audioPlayer.isPlaying == false && _body.velocity.magnitude > 0)
+         {
+             audioPlayer.volume = Random.Range(.8f, 1f);
+             audioPlayer.pitch = Random.Range(.9f, 1f);
+             audioPlayer.Play();
+         }
+
+
+     }
+
+     private void OnJump(InputAction.CallbackContext obj)
     {
         if (!_isGrounded) return;
         _body.AddForce(Vector3.up * jumpForce);
