7c02ddd4dec6a32af4a9ae944b61874209a4999b
Orbitz4k <B00124102@mytudublin.ie>
2022-01-07 13:20:10 +0000

    Added portal

---
+++ Prototype/Assets/script/Portal.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Portal : MonoBehaviour
+{
+     void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Tele")
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
+        
+    }
+}
