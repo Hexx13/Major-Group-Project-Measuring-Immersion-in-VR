2f00e743e77668dc1404e98bf8868b70f9ecfcdc
Hexx13 <71945474+Hexx13@users.noreply.github.com>
2022-01-13 12:45:03 +0000

    Add Builds and Participants

--- Prototype/Assets/script/Portal.cs
+++ Prototype/Assets/script/Portal.cs
@@ -5,9 +5,13 @@ using UnityEngine.SceneManagement;
 
 public class Portal : MonoBehaviour
 {
+    //scene number value for teleporter, set in editor
     public int sceneNumber;
+
+    //when the user collides with the portal they are sent to a different level
      void OnCollisionEnter(Collision collision)
     {
+        //ensures that only player will trigger this
         if (collision.gameObject.tag == "Plaer")
         {
             SceneManager.LoadScene(sceneNumber);
